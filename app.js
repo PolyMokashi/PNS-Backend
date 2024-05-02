242a18b62d794cba2def3d23872b5be66fb3c7cc
Pradeep Dilip Mokashi <pradeepmokashi9@gmail.com>
2024-05-02 11:30:38 +0530

    dfknkds

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var bodyParser = require('body-parser');
 var itemRouter = require('./routers/item');
 var itemRouter2 = require('./routers/item2');
 var itemRouter3 = require('./routers/item3');
-// var itemRouter4 = require('./routers/item4');
 var itemRouter5 = require('./routers/item5');
 var itemRouter6 = require('./routers/item6');
 var itemRouter7 = require('./routers/item7');
@@ -14,20 +13,22 @@ const router = require('./routers/tracker');
 
 
 var app = express();
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 var PORT = 3000;
-var HOST_NAME = 'localhost';
-var DATABASE_NAME = 'shoppingList';
 
 
 mongoose.connect("mongodb+srv://polymokashi12:taJoZqAwYbiihk4K@cluster0.ap6oe80.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true  
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
@@ -50,6 +51,6 @@ app.use('/api', router);
 
 
 
-app.listen(PORT, function () {
-  console.log(`Listening on port http://localhost:${PORT}`);
-});
+app.listen(PORT,()=>
+  console.log(`Listening on port http://localhost:${PORT}`)
+);
